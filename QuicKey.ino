a7d2ea0f83d987d1b01faa6c917176e31e4d5094
aZiZko20 <91755869+aZiZko20@users.noreply.github.com>
2021-11-09 19:42:05 -0800

    Add files via upload

--- QuicKey.ino
+++ QuicKey.ino
@@ -13,15 +13,21 @@
 int keyboard[6][14] = {
                       {0xB1, 0x31, 0x32, 0x33, 0x34, 0x35, 0x36, 0x37, 0x38, 0x39, 0x30, 0x2D, 0x2B, 0xB2},
                       {0xB3, 0x71, 0x77, 0x65, 0x72, 0x74, 0x79, 0x75, 0x69, 0x6F, 0x70, 0x5B, 0x5D, 0x5C},
-                      {0xC1, 0x61, 0x73, 0x64, 0x66, 0x67, 0x68, 0x6A, 0x6B, 0x6C, 0x3B, 0x22, 0xB0      }, //13 columns here
+                      {0xC1, 0x61, 0x73, 0x64, 0x66, 0x67, 0x68, 0x6A, 0x6B, 0x6C, 0x3B, 0x22, 0x00, 0xB0}, //13 columns here
                       {0x81, 0x7A, 0x78, 0x63, 0x76, 0x62, 0x6E, 0x6D, 0x2C, 0x2E, 0x2F, 0x85            }, //12 columns here
-                      {0x80, 0x83, 0x82,     0x20     , 0x86, 0xDA, 0x84, 0xD8                           }, //FN turned to LEFT arrow button and Menu turned to UP arrow. 8 columns here
-                      {0x00, 0x00, 0x00, 0x00, 0x7B, 0x28, 0x3C, 0x3E, 0x29, 0x7D, 0x00, 0x00, 0x00, 0x00}
+                      {0x80, 0x83, 0x82, 0x00, 0x00,    0x20     , 0x00, 0x00, 0x00, 0x00, 0x86, 0x87, 0x84, 0xD8                }, //FN turned to LEFT arrow button and Menu turned to UP arrow. 8 columns here
+                      {0x00, 0x00, 0x7B, 0x28, 0x3C, 0x3E, 0x29, 0x7D}
                       };
                       
 
 int port_rows[6] = {18, 19, 20, 21, 22, 23};
-int port_cols[14] = {0, 1, 2, 3, 5, 8, 9, 10, 13, 14, 15, 16, 17, 30};
+int port_cols[14] = {17, 15, 16, 14, 3, 2, 0, 1, 30, 8, 9, 10, 5, 13};
+
+unsigned long lastDebounceTime = 0;
+unsigned long debounceDelay = 5;
+
+int buttonState;
+int lastButtonState = 0x00;
 
 //setting up inputs and outputs.
 void setup() {
@@ -76,16 +82,60 @@ int keyboard_scan(){
     }
     digitalWrite(port_rows[i], HIGH); //set the row back to 1 and move on to the next row
   }
-  return 0xFFF; //If nothing is found, return 0xFFF
+  return 0x00; //If nothing is found, return 0xFFF
 }
 
 
 void loop() {
+  
   int key = keyboard_scan();
-  if(key != 0xFFF) {
+  if(key != 0x00) {
+    
     Keyboard.press(key); //sends pressed character
     delay(50); //debounce
+    //Keyboard.releaseAll();
+    int lastKey = key;
   }
   else
     Keyboard.releaseAll();
 }
+
+
+//TEST DEBOUNCE CODE ----------------------------------------
+/*
+  // read the state of the switch into a local variable:
+  int reading = keyboard_scan();
+  //reading = 0x00 if nothing pressed
+  //reading > 0x00 if something pressed
+  
+
+  // check to see if you just pressed the button
+  // (i.e. the input went from LOW to HIGH), and you've waited long enough
+  // since the last press to ignore any noise:
+
+  // If the switch changed, due to noise or pressing:
+  if (reading != lastButtonState) {
+    // reset the debouncing timer
+    lastDebounceTime = millis();
+  }
+
+  if ((millis() - lastDebounceTime) > debounceDelay) {
+    // whatever the reading is at, it's been there for longer than the debounce
+    // delay, so take it as the actual current state:
+
+    // if the button state has changed:
+    if (reading != buttonState) {
+      buttonState = reading;
+
+      Keyboard.press(buttonState);
+     }
+
+     else {
+      Keyboard.releaseAll();
+    }
+  }
+
+  // save the reading. Next time through the loop, it'll be the lastButtonState:
+  lastButtonState = reading;
+}
+*/
